71cd07661ff9a9b69fcf17f13cec771d17140c36
Alexander Belavsky <a.belavsky97@gmail.com>
2017-10-26 18:12:31 +0300

    Add user service

---
+++ backend/src/main/java/com/netcracker/devschool/dev4/etalon/service/UserService.java
@@ -0,0 +1,18 @@
+package com.netcracker.devschool.dev4.etalon.service;
+
+import com.netcracker.devschool.dev4.etalon.entity.User;
+
+import java.util.List;
+
+
+public interface UserService {
+    public User create(User user);
+
+    public User delete(long id) throws Exception;
+
+    public List<User> findAll();
+
+    public User update(User user) throws Exception;
+
+    public User findById(long id);
+}
---
+++ backend/src/main/java/com/netcracker/devschool/dev4/etalon/service/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.netcracker.devschool.dev4.etalon.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.netcracker.devschool.dev4.etalon.entity.User;
+import com.netcracker.devschool.dev4.etalon.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserRepository userRepository;
+
+
+    @Override
+    @Transactional
+    public User create(User user) {
+        User createdUser = user;
+        return userRepository.save(createdUser);
+    }
+
+    @Override
+    @Transactional
+    public User delete(long id) throws Exception {
+        User deletedUser = userRepository.findOne((int) id);
+
+        if (deletedUser == null)
+            throw new Exception("Not found");
+
+        userRepository.delete(deletedUser);
+        return deletedUser;
+    }
+
+    @Override
+    @Transactional
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public User update(User user) throws Exception {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public User findById(long id) {
+        return userRepository.findOne((int) id);
+    }
+}
+
+
