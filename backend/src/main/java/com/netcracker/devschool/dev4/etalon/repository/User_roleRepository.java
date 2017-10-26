ff2d30751a1cd1478fb2c8d10c88665bf0c85098
Alexander Belavsky <a.belavsky97@gmail.com>
2017-10-26 18:41:37 +0300

    User role repository

---
+++ backend/src/main/java/com/netcracker/devschool/dev4/etalon/repository/User_roleRepository.java
@@ -0,0 +1,7 @@
+package com.netcracker.devschool.dev4.etalon.repository;
+
+import com.netcracker.devschool.dev4.etalon.entity.User_role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface User_roleRepository extends JpaRepository<User_role, Integer> {
+}
